ed304ac3318432ca026157fb4e1eb2804d7023b7
tesla33io <artemstavropolov@gmail.com>
2023-10-31 20:49:35 +0100

    fix: refactor `commands.py`.

--- modules/commands.py
+++ modules/commands.py
@@ -29,25 +29,31 @@ async def on_start(message: Message):
     messages.load_data()
     daily_words.load_data()
     user_id = message.from_user.id
+
     if user_id in users and user_id in subscribed_users:
         try:
             word = daily_words[datetime.now().strftime("%d.%m.%Y")]
-            word["article"] = markdown_escape(word["article"]) if word["article"] else ""
-            word["explanations"] = [markdown_escape(expl) for expl in word["explanations"]]
+            word["article"] = (
+                markdown_escape(word["article"]) if word["article"] else ""
+            )
+            word["explanations"] = [
+                markdown_escape(expl) for expl in word["explanations"]
+            ]
             word["examples"] = [markdown_escape(examp) for examp in word["examples"]]
+
+            emoji = random.choice(messages["emojies"]["list"])
+            text = messages["todays_word"]["text"].format(emoji=emoji, word=word)
+            reply_markup = get_mailing_menu(
+                word=word.get("word", ""), sect_from="start"
+            )
             await bot.send_message(
-                chat_id=message.chat.id,
-                text=messages["todays_word"]["text"].format(
-                    emoji=random.choice(messages["emojies"]["list"]), word=word
-                ),
-                reply_markup=get_mailing_menu(word=word["word"], sect_from="start"),
+                chat_id=message.chat.id, text=text, reply_markup=reply_markup
             )
         except KeyError:
             await bot.send_message(
-                chat_id=message.chat.id,
-                text=messages["no_word_for_today"]["text"]
+                chat_id=message.chat.id, text=messages["no_word_for_today"]["text"]
             )
-    elif user_id in users and user_id not in subscribed_users:
+    elif user_id in users:
         subscribed_users[user_id] = subscriber_document_template
         subscribed_users[user_id].save()
         await bot.send_message(
@@ -62,9 +68,10 @@ async def on_start(message: Message):
         users[user_id].save()
         subscribed_users[user_id] = subscriber_document_template
         subscribed_users[user_id].save()
+        welcome_text = messages["welcome"]["text"].format(user=message.from_user)
         await bot.send_message(
             chat_id=message.chat.id,
-            text=messages["welcome"]["text"].format(user=message.from_user),
+            text=welcome_text,
         )
 
 
@@ -75,21 +82,24 @@ async def on_unsubscribe(message: Message):
     if user_id in subscribed_users:
         subscribed_users.delete(user_id)
         if user_id not in subscribed_users:
+            success_message = messages["unsubscribe_success"]["text"]
             await bot.send_message(
                 chat_id=user_id,
-                text=messages["unsubscribe_success"]["text"],
+                text=success_message,
             )
             return
         else:
             logger.error(f"Failed unsubscription. User id: {user_id}")
+            fail_message = messages["unsubscribe_fail"]["text"]
             await bot.send_message(
                 chat_id=user_id,
-                text=messages["unsubscribe_fail"]["text"],
+                text=fail_message,
             )
             return
+    already_unsubscribed_message = messages["unsubscribe_already"]["text"]
     await bot.send_message(
         chat_id=user_id,
-        text=messages["unsubscribe_already"]["text"],
+        text=already_unsubscribed_message,
     )
 
 
@@ -100,29 +110,26 @@ async def on_fix_translate(message: Message):
     # this function should fix it
     words.load_data()
     daily_words.load_data()
+    await fix_translate_data(words, "All words")
+    await fix_translate_data(daily_words, "Daily words")
+
+
+async def fix_translate_data(data, data_name):
+    data.load_data()
     i = 0
     corrected_words = []
-    corrected_words.append("All words:")
-    for word in words:
+    for word, word_data in data.items():
         logger.debug(word)
-        new_translations = fix_translate(words[word]["translations"])
-        if new_translations != words[word]["translations"]:
+        new_translations = fix_translate(word_data.get("translations", []))
+        if new_translations != word_data.get("translations"):
             i += 1
             corrected_words.append(word)
-        words[word]["translations"] = new_translations
-        words[word].save()
-    corrected_words.append("\nDaily words:")
-    for word in daily_words:
-        logger.debug(word)
-        new_translations = fix_translate(daily_words[word]["translations"])
-        if new_translations != daily_words[word]["translations"]:
-            i += 1
-            corrected_words.append(word)
-        daily_words[word]["translations"] = new_translations
-        daily_words[word].save()
-    await bot.send_message(
-        chat_id=int(os.getenv("ADMIN")),
-        text=markdown_escape(
-            f"Corrected: {i} documents.\n" + ", ".join(w for w in corrected_words)
+        word_data["translations"] = new_translations
+        word_data.save()
+    if i > 0:
+        await bot.send_message(
+            chat_id=int(os.getenv("ADMIN")),
+            text=markdown_escape(
+                f"Corrected {data_name}: {i} documents.\n" + ", ".join(corrected_words)
+            ),
         )
-    )
